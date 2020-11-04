7e46064f22659dd402e0636fc242446365434416
eightfeet <bajiao000000@163.com>
2020-11-04 11:58:19 +0800

    update

--- deployPro.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PUBLIC_PATH=http://cdn-yyj.4000916916.com/yourpath
-yarn
-yarn build
-#scp -r ./build/* ...
--- deployUat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-npm run build:uat
-scp -r ./build/* www@192.168.103.107:~/h5/antifake
