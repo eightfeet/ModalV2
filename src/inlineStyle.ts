67c2a490a9e5fc9018ee0df02c7fcec7731f7327
eightfeet <bajiao000000@163.com>
2020-11-05 15:43:53 +0800

    update

--- src/inlineStyle.ts
+++ src/inlineStyle.ts
@@ -3,12 +3,13 @@ import { cssifyObject } from 'css-in-js-utils';
 import * as CSS from 'csstype';
 
 /**
- *
- * 创建行内样式
+ * StyleObject样式转换行内样式
  * @param {CSS.Properties} style
+ * @return {*}  {(string | null)}
  */
-const createInlineStyles = (style: CSS.Properties) => {
-    return cssifyObject(prefix(style));
+const createInlineStyles = (style: CSS.Properties): string | null => {
+    const css = cssifyObject(prefix(style));
+    return (css ? `${css};` : null);
 };
 
 export { createInlineStyles };
